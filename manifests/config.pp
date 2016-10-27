965231c7033540f34714c959682de28e11e8244e
Jordi Prats <jprats@systemadmin.es>
2016-10-27 13:02:11 +0200

    config file

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,4 +4,12 @@
 #
 class squid::config inherits squid {
 
+  file { $squid::params::config_file:
+    ensure => 'present',
+    owner => 'root',
+    group => 'squid',
+    mode => '0640',
+    content => template("${module_name}/squidconf.erb"),
+  }
+
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -3,13 +3,24 @@
 # === squid documentation
 #
 class squid (
-              $port                  = '3128',
-              $manage_package        = true,
-              $package_ensure        = 'installed',
-              $manage_service        = true,
-              $manage_docker_service = true,
-              $service_ensure        = 'running',
-              $service_enable        = true,
+              $port                          = '3128',
+              $httpd_suppress_version_string = true,
+              $add_via_header                = false,
+              $add_forwarded_for_header      = false,
+              $coredump_dir                  = '/var/spool/squid',
+              $localnet                      = [ '10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16', 'fc00::/7', 'fe80::/10' ],
+              $ssl_ports                     = [ '443' ],
+              $safe_ports                    = [ '80', '21', '443' ],
+              $unsafeports_action            = 'deny',
+              $localnet_action               = 'allow',
+              $localhost_action              = 'allow',
+              $default_action                = 'deny',
+              $manage_package                = true,
+              $package_ensure                = 'installed',
+              $manage_service                = true,
+              $manage_docker_service         = true,
+              $service_ensure                = 'running',
+              $service_enable                = true,
             ) inherits squid::params{
 
   class { '::squid::install': } ->
