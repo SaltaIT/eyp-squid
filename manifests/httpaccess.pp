55a6214460ffc08e55a3e65ed1197f215fe6e17f
Jordi Prats <jprats@systemadmin.es>
2017-01-30 14:43:38 +0100

    lint

--- manifests/httpaccess.pp
+++ manifests/httpaccess.pp
@@ -1,4 +1,15 @@
-# http_access allow localnet RHEL-UpdateServers
-define squid::htppaccess() {
-  # concat 40
+# http_access allow RHEL-UpdateServers
+# http_access allow|deny [!]aclname ..
+define squid::httpaccess(
+                          $aclname        = $name,
+                          $action         = 'allow',
+                          $inverse        = false,
+                          $description    = undef,
+                          $order          = '0',
+                        ) {
+  concat::fragment{ "${squid::params::config_file} httpaccess ${aclname}/${name}":
+    target  => $squid::params::config_file,
+    order   => "40${order}",
+    content => template("${module_name}/http_access.erb"),
+  }
 }
